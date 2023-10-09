61df623856530ed9a8adf6e8fa4780fdf0fbb33b
lai2-01 <lai2-01@itismeucci.com>
2023-10-09 10:14:03 +0200

    implemented client

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import java.net.Socket;
+import java.io.*;
+import java.net.*;
 /**
  * Hello world!
  *
@@ -8,5 +11,26 @@ public class App
 {
     public static void main( String[] args )
     {
+        try {
+            // leggo dalla tastiera
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            String riga = in.readLine();
+
+            // avvio la communicazione con socket
+            Socket socket = new Socket("10.22.9.13", 3000);
+
+            // invio la stinga messa dal utente
+            DataOutputStream outVersoServer = new DataOutputStream(socket.getOutputStream());
+            outVersoServer.writeBytes(riga+"\n");
+
+            // metto in ascolto per leggere la riga dal server
+            BufferedReader inDalServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String rigaRitornata = inDalServer.readLine();
+
+            System.out.println("stringa ritornata : " + rigaRitornata);
+            
+        } catch (Exception e) {
+            System.out.println("Errore");
+        }
     }
 }
